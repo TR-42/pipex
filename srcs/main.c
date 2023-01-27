16c99e0e91c9be168813d6894210226bc4054792
kfujita <kfujita@student.42tokyo.jp>
2023-01-28 07:21:36 +0900

    Create main.c
    とりあえず、テスト用に環境変数を出力したりするだけの実装。

---
+++ srcs/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 22:45:11 by kfujita           #+#    #+#             */
+/*   Updated: 2023/01/28 07:16:07 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/pipex.h"
+#include "../headers/print_error.h"
+#include "../headers/env_util.h"
+#include "../libft/ft_printf/ft_printf.h"
+
+int	main(int argc, const char *argv[], const char *envp[])
+{
+	if (argc < REQUIRED_ARGC)
+		print_err_exit(ERR_HELP_MSG, ERR_HELP_EXITNUM);
+	(void)argv;
+	ft_printf("PATH = %s\n", get_env_value(envp, "PATH"));
+	ft_printf("TMPDIR = %s\n", get_env_value(envp, "TMPDIR"));
+	ft_printf("SHELL = %s\n", get_env_value(envp, "SHELL"));
+	ft_printf("~~~~~~~~~~~~~~~~~~~~~~~~~");
+	while (*envp != NULL)
+	{
+		ft_printf("%s\n", *envp);
+		envp++;
+	}
+	return (0);
+}
