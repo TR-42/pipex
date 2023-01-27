71746b4ac983729e2b4bb33ca691740046c39d60
kfujita <kfujita@student.42tokyo.jp>
2023-01-28 07:20:26 +0900

    Create pipex.h

---
+++ headers/pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 22:45:53 by kfujita           #+#    #+#             */
+/*   Updated: 2023/01/28 07:15:08 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# define REQUIRED_ARGC 5
+
+# define ERR_HELP_EXITNUM 2
+# define ERR_HELP_MSG "./pipex file1 cmd1 cmd2 file2\n"
+
+#endif
