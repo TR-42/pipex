e2c29162752a8191c04ae6970fa0281cf3d83059
kfujita <kfujita@student.42tokyo.jp>
2023-01-31 15:18:18 +0900

    `envp`の型を`execve`に合わせた

--- headers/env_util.h
+++ headers/env_util.h
@@ -6,14 +6,14 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 06:39:47 by kfujita           #+#    #+#             */
-/*   Updated: 2023/01/30 16:10:48 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/01/31 13:47:37 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_UTIL_H
 # define ENV_UTIL_H
 
-const char	*get_env_value(const char *envp[], const char *name);
-char	**get_path_in_env(const char *envp[]);
+const char	*get_env_value(char *const envp[], const char *name);
+char		**get_path_in_env(char *const envp[]);
 
 #endif
--- srcs/env_util.c
+++ srcs/env_util.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 06:42:05 by kfujita           #+#    #+#             */
-/*   Updated: 2023/01/30 16:09:26 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/01/31 14:05:29 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-static const char	*is_this_requested_env(const char *envp, const char *name);
+static const char	*is_this_requested_env(char *const envp, const char *name);
 
-const char	*get_env_value(const char *envp[], const char *name)
+const char	*get_env_value(char *const envp[], const char *name)
 {
 	const char	*p_value;
 
@@ -33,7 +33,7 @@ const char	*get_env_value(const char *envp[], const char *name)
 	return (p_value);
 }
 
-static const char	*is_this_requested_env(const char *envp, const char *name)
+static const char	*is_this_requested_env(char *envp, const char *name)
 {
 	if (envp == NULL)
 		return (NULL);
@@ -50,7 +50,7 @@ static const char	*is_this_requested_env(const char *envp, const char *name)
 		return (NULL);
 }
 
-char	**get_path_in_env(const char *envp[])
+char	**get_path_in_env(char *const envp[])
 {
 	const char	*path;
 
