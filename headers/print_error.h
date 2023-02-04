ed37758bec970428fde0d72b6d20e908d979407a
kfujita <kfujita@student.42tokyo.jp>
2023-02-04 19:20:42 +0900

    `perror_exit`関数を追加

--- headers/print_error.h
+++ headers/print_error.h
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 22:48:17 by kfujita           #+#    #+#             */
-/*   Updated: 2023/01/28 07:08:30 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/02/04 11:06:53 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define PRINT_ERROR_H
 
 void	print_err_exit(const char *msg, const int exit_code);
+void	perror_exit(const char *msg);
 
 #endif
--- srcs/print_error.c
+++ srcs/print_error.c
@@ -6,18 +6,30 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 22:49:32 by kfujita           #+#    #+#             */
-/*   Updated: 2023/01/28 07:12:09 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/02/04 17:46:47 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/print_error.h"
 #include "../libft/ft_string/ft_string.h"
 
+// - perror
+#include <stdio.h>
+
+// - exit
 #include <stdlib.h>
+
+// - write
 #include <unistd.h>
 
 void	print_err_exit(const char *msg, const int exit_code)
 {
 	write(STDERR_FILENO, msg, ft_strlen(msg));
 	exit(exit_code);
+}
+
+void	perror_exit(const char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
 }
